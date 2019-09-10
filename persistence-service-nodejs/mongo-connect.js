ca81b34d2dc715201bb83040c78e4c3ef9c1794d
Billy Vlachos <williamcaminiti@live.com>
2019-09-10 12:42:26 +0200

    Edit mongo connect js file

--- persistence-service-nodejs/mongo-connect.js
+++ persistence-service-nodejs/mongo-connect.js
@@ -1,16 +1,25 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://lakylekidd:Hermes%401986@cluster0-aysvk.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
+// Import required modules
+const mongoose = require('mongoose');
+const vehicleData = require('./schemas/vehicle-data.schema');
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-    // Connection successful
-    var vehicleDataSchema = new mongoose.Schema({
-        time: Number,
-        energy: Number,
-        gps: [],
-        odo: Number,
-        speed: Number,
-        soc: Number
-    });
-});
+// Function that returns a connected mongoose object
+const connectDb = () => {
+    // Return a connected mongoose to specific connection string
+    return mongoose.connect('mongodb+srv://lakylekidd:Hermes%401986@cluster0-aysvk.mongodb.net/test?retryWrites=true&w=majority');
+}
+
+// Variable that holds the available models
+const models = { vehicleData };
+
+// Export the module
+module.exports = models, { connectDb };
+
+
+// mongoose.connect('', { useNewUrlParser: true });
+
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function () {
+//     // Connection successful
+
+// });
