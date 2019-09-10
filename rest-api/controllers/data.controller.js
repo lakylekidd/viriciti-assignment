fa80574a1871ba90bf5578a5f87fe531f946979d
Billy Vlachos <williamcaminiti@live.com>
2019-09-10 15:44:22 +0200

    Add unimplemented functions to controller

---
+++ rest-api/controllers/data.controller.js
@@ -0,0 +1,17 @@
+
+
+/**
+ * Returns a list of vehicle data by vehicle name
+ */
+const getByVehicleName = (req, res) => {
+    throw new Error("Not implemented");
+}
+/**
+ * Returns a single entry of vehicle data by ID
+ */
+const getById = (req, res) => {
+    throw new Error("Not implemented");
+}
+
+// Export the functions
+module.exports = { getById, getByVehicleName }
