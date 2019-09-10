7b217e0b75ea4045f770063f7a254aa15c0f7032
Billy Vlachos <williamcaminiti@live.com>
2019-09-10 12:21:18 +0200

    Remove url from nats connect

--- persistence-service-nodejs/vehicle-stream.js
+++ persistence-service-nodejs/vehicle-stream.js
@@ -1,36 +1,32 @@
 // Import required modules
-const NATS = require("nats");
+const NATS = require('nats');
 
 module.exports = (logger) => {
+	connect: () => {
+		// ** NATS CONFIG ** //
+		const reconnecting = false;
 
-    connect: () => {
-        // ** NATS CONFIG ** //
-        const reconnecting = false;
+		// Connect NATS
+		const nats = NATS.connect({
+			json: true
+		});
+		// Verify NATS connection
+		nats.on('connect', () => {
+			logger.info('NATS is now online!');
+		});
+		// Listen for messages from publisher
+		nats.subscribe('vehicle.test-bus-1', (data) => {
+			// Check if connected
+			if (!reconnecting) {
+				// Add data to the database
+				addToDatabase(data);
+			}
+		});
 
-        // Connect NATS
-        const nats = NATS.connect({
-            json: true,
-            //url: 'nats://localhost:4222'
-        });
-        // Verify NATS connection
-        nats.on('connect', () => {
-            logger.info("NATS is now online!");
-        });
-        // Listen for messages from publisher
-        nats.subscribe('vehicle.test-bus-1', (data) => {
-            // Check if connected
-            if (!reconnecting) {
-                // Add data to the database
-                addToDatabase(data);
-            }
-        });
-
-        /**
+		/**
          * Adds the received data to the database
          * @param {Object} data The received data
          */
-        const addToDatabase = (data) => {
-
-        }
-    };
-}
+		const addToDatabase = (data) => {};
+	};
+};
