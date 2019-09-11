dc4f2e9f454929a3446f861fe09a133777417422
Billy Vlachos <williamcaminiti@live.com>
2019-09-11 18:27:23 +0200

    Simple server file

---
+++ websocket-api/src/server.ts
@@ -0,0 +1,27 @@
+// Import required modules
+import * as express from 'express';
+import * as http from 'http';
+import * as WebSocket from 'ws';
+
+// Initialize the express app
+const app = express();
+
+// Initialize a simple http server
+const server = http.createServer(app);
+
+// Initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+// Add on connect event to web socket server
+wss.on('connection', (ws: WebSocket) => {
+
+    // WS Server is connected -- Send feedback
+    ws.send(`You have connected to the WebSocket Server!`);
+});
+
+// Start our server
+server.listen(5000, () => {
+
+    // Log server is listening
+    console.log(`Server started on port 5000`);
+});
