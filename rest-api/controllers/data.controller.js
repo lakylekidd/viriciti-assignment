91f77061b4729075b4f4c871b97e23a34ba0282f
Billy Vlachos <williamcaminiti@live.com>
2019-09-11 14:28:56 +0200

    Implemed get by ID

--- rest-api/controllers/data.controller.js
+++ rest-api/controllers/data.controller.js
@@ -16,7 +16,7 @@ const getByVehicleName = (req, res) => {
     if (limit) { query = query.limit(parseInt(limit)); }
     // Check if skip is specified
     if (skip) { query = query.skip(parseInt(skip)); }
-
+    // Return the search result
     return query
         .exec()
         .then(result => {
@@ -35,7 +35,32 @@ const getByVehicleName = (req, res) => {
  * Returns a single entry of vehicle data by ID
  */
 const getById = (req, res) => {
-    throw new Error("Not implemented");
+    // Retrive the entry id
+    const id = req.query.id;
+
+    // Generate the find query
+    let query = models.VehicleData.findById(id);
+
+    // Return the search result
+    return query
+        .exec()
+        .then(result => {
+            // Check if object was found
+            if (result) {
+                // Return the resulted object
+                return res.status(200).json(JSON.stringify(result));
+            } else {
+                // Return 404 not found
+                return res.status(404).json({
+                    message: `The data object with id ${id} does not exist.`
+                })
+            }
+
+        })
+        .catch(err => {
+            // Log error and return it
+            return res.status(500).json({ error: err })
+        });
 }
 
 // Export the functions
