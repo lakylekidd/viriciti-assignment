366676351fbb43ce75005b89f59c7317c2329309
Billy Vlachos <williamcaminiti@live.com>
2019-09-11 15:49:21 +0200

    Rename test file and folders for discovery

--- rest-api/test/vehicle-data.js
+++ rest-api/test/vehicle-data.js
@@ -2,12 +2,12 @@
 process.env.NODE_ENV = "test";
 
 const mongoose = require('mongoose');
-const VehicleData = require('./../schemas/vehicle-data.schema');
+const VehicleData = require('../schemas/vehicle-data.schema');
 
 // Load the test dependencies
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('./../server');
+const server = require('../server');
 const should = chai.should();
 
 // Test data
