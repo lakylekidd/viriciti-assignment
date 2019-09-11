447322ab7220b9046d2ce18952e2a0c02d567f15
Billy Vlachos <williamcaminiti@live.com>
2019-09-11 19:49:57 +0200

    Update module requirements for testing

--- websocket-api/src/schemas/vehicle-data.schema.spec.ts
+++ websocket-api/src/schemas/vehicle-data.schema.spec.ts
@@ -1,21 +1,25 @@
 // Import required modules
 import * as mongoose from 'mongoose';
+import { expect } from 'chai';
 import VehicleData, { IVehicleData } from './vehicle-data.schema';
 
 // Begin Vehicle Data Model Test
 describe("Vehicle Data Model", () => {
-
     // To be performed before tests are ran
     before(async () => {
         // Connect to in-memory database
-        await mongoose.connect("", {
+        await mongoose.connect("mongodb+srv://lakylekidd:Hermes%401986@cluster0-aysvk.mongodb.net/test?retryWrites=true&w=majority", {
             useNewUrlParser: true
         });
     });
-
     // To be performed after tests are ran
     after(async () => {
         // Close connection
         mongoose.connection.close();
     });
+    // The empty vehicle data model should throw validation error
+    it("Should throw validation errors", () => {
+        const data = new VehicleData();
+        expect(data.validate).to.throw();
+    })
 })
